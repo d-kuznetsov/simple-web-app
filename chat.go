795f4c2eea0287941d999af12b81906579c1686e
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-08-03 11:02:56 +0200

    create logger

--- chat.go
+++ chat.go
@@ -5,10 +5,28 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+)
+
+var (
+	WarningLogger *log.Logger
+	InfoLogger    *log.Logger
+	ErrorLogger   *log.Logger
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there !!!, I love %s!", r.URL.Path[1:])
+}
+
+func init() {
+	file, err := os.OpenFile("logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	InfoLogger = log.New(file, "INFO: ", log.Ldate|log.Ltime|log.Lshortfile)
+	WarningLogger = log.New(file, "WARNING: ", log.Ldate|log.Ltime|log.Lshortfile)
+	ErrorLogger = log.New(file, "ERROR: ", log.Ldate|log.Ltime|log.Lshortfile)
 }
 
 func main() {
@@ -16,6 +34,11 @@ func main() {
 	flag.StringVar(&port, "port", "8080", "port")
 	flag.Parse()
 	fmt.Printf("%T %s", port, port)
+
+	InfoLogger.Println("Starting the application...")
+	InfoLogger.Println("Something noteworthy happened")
+	WarningLogger.Println("There is something you should know about")
+	ErrorLogger.Println("Something went wrong")
 
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
