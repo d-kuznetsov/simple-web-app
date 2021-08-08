2712476764342d08e1b652d5809875d00bcf4b26
Dmitry Kuznetsov <ds.kuznetsov93@yandex.ru>
2021-08-08 12:25:28 +0200

    add redirection by submit

--- main.go
+++ main.go
@@ -8,16 +8,48 @@ import (
 	"github.com/d-kuznetsov/chat/config"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.New("").ParseFiles("templates/home.html", "templates/layout.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	tmpl.ExecuteTemplate(w, "layout", nil)
-}
-
 func main() {
-	http.HandleFunc("/", handler)
+	templates := map[string]*template.Template{
+		"login": template.Must(template.ParseFiles("templates/layout.html", "templates/login.html")),
+		"home":  template.Must(template.ParseFiles("templates/layout.html", "templates/home.html")),
+	}
+	renderTemplate := func(w http.ResponseWriter, tmplName string, tmplData interface{}) {
+		tmpl, ok := templates[tmplName]
+		if !ok {
+			log.Fatal("There is not the template")
+		}
+		err := tmpl.Execute(w, tmplData)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		renderTemplate(w, "home", nil)
+	})
+	http.HandleFunc("/login", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			if r.FormValue("username") == "user" && r.FormValue("password") == "1234" {
+				http.Redirect(w, r, "/", http.StatusFound)
+				return
+			}
+			renderTemplate(w, "login", LoginDetails{
+				r.FormValue("username"),
+				r.FormValue("password"),
+				"Data is incorrect",
+			})
+			return
+		}
+
+		renderTemplate(w, "login", nil)
+	})
+
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	log.Fatal(http.ListenAndServe(":"+config.Port, nil))
+}
+
+type LoginDetails struct {
+	Username string
+	Password string
+	Error    string
 }
